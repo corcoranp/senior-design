13e70b63b6dcf49beb4786fefcc773cdc38d3ed7
Peter Corcoran <pmcorcor@gmail.com>
2015-06-30 22:59:17 -0500

    Added threading, and basic logging facilities... Logger will write out to console, or to file, at specific log level...

--- sourcecode/source/bootloader.cpp
+++ sourcecode/source/bootloader.cpp
@@ -6,27 +6,46 @@
  * Initial c++ coding for 2015-2016 UAB EE Senior Design
  *
  */
+#include <iostream>
 
+#include <pthread.h>		//Needed for threading
+#include <stdio.h>			//Needed for threading
+#include <unistd.h>			//Needed for usleep
 
-
-//Needed for threading
-//#include <thread>
-//Needed for threading
-#include <stdio.h>
-
+/*
+ * BLAZEBOT INCLUDES
+ */
 #include "../include/robotcore.h"
 #include "../include/qrcode.h"
 #include "../include/zone.h"
-
-
 #include "../include/sysops.h"
+#ifdef LOG1
+#include "log1.h"
+#else
+#include "log.h"
+#endif
 
-/*
- * Namespace Declaration
- */
+//used for command options
+#include <algorithm>
+
 using namespace std;
 
+/*
+ * DECLARATIONS - MY FUNCTIONS
+ */
+void bootSystemOperations();
+
+//Commandline options
+char* getCmdOption(char ** begin, char ** end, const std::string & option);
+bool cmdOptionExists(char** begin, char** end, const std::string& option);
+
+//DEBUG OPTIONS---------
 void print(string);
+void debug(string);
+void info(string);
+FILE * log_file;
+
+void showBanner();
 
 /*
  * BlazeBot Bootloader Main Control
@@ -41,37 +60,135 @@ void print(string);
  * need to be done, then sysops.start() can be executed, and extended
  * to inject any system dependencies/settings.
  */
- int main(void)
+ int main(int argc, char* argv[])
 {
-	 print("EE498 - Senior Design");
-	 print("Bootloader Started");
-/*
-	 QRCode q;
+//	 cout << "\x1B[2J\x1B[H";					//clear screen
+	char * item;
+	//std::string Str = std::string(cStr);
+	try
+	{
+
+		char * logLevel = getCmdOption(argv, argv + argc, "-logLevel");
+		if(logLevel){
+			FILELog::ReportingLevel() = FILELog::FromString(logLevel ? logLevel : "DEBUG1");
+		}
+		//write to log
+		if(cmdOptionExists(argv, argv+argc, "-log")){
+			LOG_TO_FILE(log_file,"./blazebot.log");
+		}
 
 
-	 q.getCode();
+		showBanner();
+		bootSystemOperations();
 
-	 Color red = RED; //example ENUM value..
+	}
+	catch(const std::exception& e)
+	{
+		FILE_LOG(logERROR) << e.what();
+	}
 
-	 map <Color, string> c;
-
-
-	 cout << c[RED];
-
-
-	 Zone z;
-	 cout << z.color();
-
-*/
-	 //SYSOPS STARTUP~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-	 //thread_create(&thread_id,NULL,(sysops::entry),new sysops());
+	//write log file out:
+	EXIT_LOGGER(log_file);
     return 0;
 }
 
 
+
+
+void bootSystemOperations(){
+debug("started bootloader::bootSystemOperations");
+	 //STEP 1: Define Handle to the Thread:
+	 pthread_t sysopsEntry;
+	 //OPTIONAL: Define the data received back from pthread.
+	 void* result;
+	 //STEP 2: Create thread, pass reference, addr of the function and data
+	 if(pthread_create(&sysopsEntry, NULL, &sysops::entry, NULL)){
+		 cout <<"Failed to create the thread" << endl;
+		 return;
+	 }
+	 //STEP 3: Allow the Thread to complete...
+	 pthread_join (sysopsEntry, &result);
+
+	 return;
+}
+
+
+
+
+/*
+ * Generic Method for printing text to the console
+ */
  void print (string msg){
 	 cout << msg << endl;
+ }
+ void debug (string msg){
+	 FILE_LOG(logDEBUG) << msg;
+ }
+ void info (string msg){
+	 FILE_LOG(logINFO) << msg;
+ }
+
+ char* getCmdOption(char ** begin, char ** end, const std::string & option)
+ {
+     char ** itr = std::find(begin, end, option);
+     if (itr != end && ++itr != end)
+     {
+         return *itr;
+     }
+     return 0;
+ }
+
+ bool cmdOptionExists(char** begin, char** end, const std::string& option)
+ {
+     return std::find(begin, end, option) != end;
+ }
+
+
+
+
+
+
+
+
+
+
+/*
+ * BLAZE BOT BANNER TEXT
+ */
+ void showBanner(){
+	 info("                     _                    _");
+	 info("                  ,/                        \\,");
+	 info("        _________{(                          })_________");
+	 info("       /.-------./\\                        //\\.-------.\\");
+	 info("      //@@@@@@@//@@\\\\  )                (  //@@\\\\@@@@@@@\\\\");
+	 info("     //@@@@@@@//@@@@>>/                  \\<<@@@@\\\\@@@@@@@\\\\");
+	 info("    //O@O@O@O//@O@O//                      \\\\O@O@\\\\O@O@O@O\\\\");
+	 info("  //OOOOOOOO//OOOO||          \\  /          ||OOOO\\\\OOOOOOOO\\\\");
+	 info(" //O%O%O%O%//O%O%O%\\\\         ))((         //%O%O%O\\\\%O%O%O%O\\\\");
+	 info("||%%%%%%%%//'  `%%%%\\\\       //  \\       //%%%%'   `\\\\%%%%%%%||");
+	 info("((%%%%%%%((      %%%%%\\\\    ((    ))    //%%%%%       ))%%%%%%))");
+	 info(" \\\\:::' `::\\      `:::::\\\\   \\)~~(/    //:::::'      //::' `:::/");
+	 info("  )'     `;)'      (`  ` \\\\ `<@  @>' / / '  ')      `(;'     `(");
+	 info("          (               \\`\\ )^^( /  /               )");
+	 info("        _                  ) \\\\oo/   (");
+	 info("       (@)                  \\  `'   /                      _");
+	 info("       |-|\\__________________\\__^__<________oOo__________ (@)");
+	 info("       |-|                                  VVV          \\|-|");
+	 info("       |-|             2016 IEEE SOUTHEASTCON             |-|");
+	 info("       |-|           UAB ELECTRICAL ENGINEERING           |-|");
+	 info("       |-|                                                |-|");
+	 info("       |-|      ¸.´¯`·.¸¸.·´¯`·.BLAZE.·´¯`·.¸¸.´¯`·.¸     |-|");
+	 info("       |-|                                                |-|");
+	 info("       |-|\\_____________________________________________  |-|");
+	 info("       (@)                 / ,/ \\_____/ \\ ~\\/~         `\\|-|");
+	 info("        ~             ___//^~      \\____/\\               (@)");
+	 info("                     <<<  \\     __  <____/||               ~");
+	 info("                               <   \\ <___/||");
+	 info("                                  || <___//");
+	 info("                                  \\ \\/__//");
+	 info("                                   ~----~");
+	 info("");
+	 info("");
+	 info("Starting Birmingham's Logistics Actuating Zone Evaluator ...");
+	 	 return;
  }
--- sourcecode/source/sysops.cpp
+++ sourcecode/source/sysops.cpp
@@ -6,20 +6,31 @@
  * Jun 27, 2015 - Initial Coding
  *
  * This file is used as the main controller system of
+ * The responsibility of this class is to control system operations
  *
  */
 #include <iostream>
 #include "../include/sysops.h"
 
+#ifdef LOG1
+#include "log1.h"
+#else
+#include "log.h"
+#endif
+
 using namespace std;
 
+
+//Thread function that will execute when the thread is created
+//it passes and receives data by void pointers
 void *sysops::entry(void *args){
-	sysops *thisCls = (sysops *)args;
-	thisCls-> start();
+	sysops *thisClass = (sysops *)args; //cast the data passed to pointer to sysops
+	thisClass->start();
 }
 
+
 void sysops::start(){
+	FILE_LOG(logDEBUG)  << "SysOps Started";
 
-
-	cout << "entered start of sysops" << endl;
+	cout << "SysOps Ended" << endl;
 }
